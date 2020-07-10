0ac8e29e909db5538e98ef0a52ece703901ce756
slroberts <shomariroberts@gmail.comclear>
2020-07-10 19:23:45 -0400

    updated styling

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,18 +1,40 @@
 import React from "react";
 import {Box} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
+
+const useStyles = makeStyles(() => ({
+  todo: {
+    fontSize: "1.5rem",
+    borderBottom: "solid thin grey",
+    paddingBottom: "1rem",
+  },
+  icon: {
+    marginRight: ".5rem",
+  },
+}));
 
 const Todo = (props) => {
+  const classes = useStyles();
+
   return (
     <Box
-      mb={3}
-      p={4}
-      bgcolor="success.main"
-      color="text.primary"
+      display="flex"
       fontFamily="fontFamily"
-      boxShadow={2}
-      borderRadius="borderRadius"
+      mb={3}
+      alignItems="center"
       onClick={() => props.toggleCompleted(props.todo.id)}
+      className={classes.todo}
     >
+      {props.todo.completed ? (
+        <CheckBoxIcon
+          className={classes.icon}
+          style={{color: "rgb(76, 175, 80)"}}
+        />
+      ) : (
+        <CheckBoxOutlineBlankIcon className={classes.icon} />
+      )}
       {props.todo.task}
     </Box>
   );
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,47 @@
 import React from "react";
-import {Box, TextField} from "@material-ui/core";
+import {Box, TextField, Grid} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  todoForm: {
+    "& .MuiFormLabel-root.Mui-focused": {
+      color: "rgb(76, 175, 80) !important",
+    },
+    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      borderColor: "rgb(76, 175, 80) !important",
+    },
+    "& button": {
+      border: "none",
+      outline: "none",
+      width: "100%",
+      marginBottom: "1rem",
+      padding: "1.25rem",
+      borderRadius: ".25rem",
+      backgroundColor: "#121212",
+      color: "white",
+      fontSize: ".9rem",
+
+      "&:hover": {
+        backgroundColor: "rgb(76, 175, 80) !important",
+        color: "#121212",
+      },
+    },
+  },
+  clearBtn: {
+    backgroundColor: "rgb(255, 152, 0) !important",
+    color: "#121212",
+  },
+}));
 
 const TodoForm = (props) => {
+  const classes = useStyles();
+
   return (
-    <form onSubmit={props.handleSubmit} autoComplete="off">
+    <form
+      onSubmit={props.handleSubmit}
+      autoComplete="off"
+      className={classes.todoForm}
+    >
       <Box mb={2} width={1}>
         <TextField
           id="task"
@@ -16,10 +54,16 @@ const TodoForm = (props) => {
           fullWidth
         />
       </Box>
-      <Box>
-        <button>Add Task</button>
-        <button onClick={props.clearCompleted}>Clear Completed Task</button>
-      </Box>
+      <Grid container justify="space-between">
+        <Grid item xs={12}>
+          <button className={classes.addBtn}>Add Task</button>
+        </Grid>
+        <Grid item xs={12}>
+          <button onClick={props.clearCompleted} className={classes.clearBtn}>
+            Clear Completed Task
+          </button>
+        </Grid>
+      </Grid>
     </form>
   );
 };
